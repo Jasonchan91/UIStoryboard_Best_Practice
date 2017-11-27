458049752d4acb0e1ca86d2e8d4d98299310cbe1
Jason Chan <j.ianchan.j@gmail.com>
2017-11-27 21:25:51 +0800

    Initial Commit

---
+++ UIStoryboard Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIStoryboard Tutorial
+//
+//  Created by Jason Chan on 27/11/17.
+//  Copyright © 2017 Jason Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
